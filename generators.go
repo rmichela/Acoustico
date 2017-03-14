f3bab9399c00760107f4bdd196a0940c373654dd
Ryan Michela <deltahat@gmail.com>
2017-03-13 21:37:11 -0700

    Implement waveform generators in terms of phasor

---
+++ generators.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"math"
+	"math/cmplx"
+)
+
+const twoPi = 2 * math.Pi
+
+// number of elapsed samples before stabilization occurs
+const stabilizationPeriod = 500
+
+// SineGenerator generates Sine waves
+type SineGenerator interface {
+	Sine(Timecode, Frequency) Amplitude
+}
+
+// CosineGenerator generates Cosine waves
+type CosineGenerator interface {
+	Cosine(Timecode, Frequency) Amplitude
+}
+
+// SawtoothGenerator generates Sawtooth waves
+type SawtoothGenerator interface {
+	Sawtooth(Timecode, Frequency) Amplitude
+}
+
+// SquareGenerator generates Square waves
+type SquareGenerator interface {
+	Square(Timecode, Frequency) Amplitude
+}
+
+// TriangleGenerator generates Triangle waves
+type TriangleGenerator interface {
+	Triangle(Timecode, Frequency) Amplitude
+}
+
+// Generator implements periodic waveform generators using a recursively defined
+// complex phasor.
+// - All waveforms are in the range [-1, 1]
+// - All waveforms (except cosine) are in phase with sine
+// -- https://en.wikipedia.org/wiki/File:Waveforms.svg
+type Generator struct {
+	// The last known generator timecode
+	t Timecode
+	// Time of last stabilization
+	s Timecode
+	// A phasor holding the current state of the trig generator
+	z complex128
+	// The sample rate for generation
+	sampleRate Frequency
+}
+
+// NewGenerator creates a new Generator
+func NewGenerator(f Frequency) *Generator {
+	gen := new(Generator)
+	gen.t = 0
+	gen.s = 0
+	gen.sampleRate = f
+	gen.z = complex(1, 0)
+	return gen
+}
+
+// Advances the phasor counterclockwise according the desired frequency with
+// respect to a change in time.
+// http://dsp.stackexchange.com/a/1087
+func (gen *Generator) advance(tʹ Timecode, f Frequency) {
+	// determine how many samples we have to advance
+	// typically this is one - more than one implies time dialation
+	Δt := tʹ - gen.t
+
+	// compute the angular frequency of the oscilator in radians
+	ω := twoPi * f / sampleRate
+
+	// compute the complex angular coeficient
+	Ω := cmplx.Exp(complex(0, ω))
+
+	// advance the phasor Δt units
+	for i := Δt; i > 0; i-- {
+		gen.z = gen.z * Ω
+	}
+
+	// stabilize the phasor's amplitude every once in a while
+	// the amplitude can drift due to rounding errors
+	// since z is a unity phasor, adjust its amplitude back towards unity
+	if gen.s > stabilizationPeriod {
+		a := real(gen.z)
+		b := imag(gen.z)
+		c := (3 - math.Pow(a, 2) - math.Pow(b, 2)) / 2
+		gen.z = gen.z * complex(c, 0)
+		gen.s = 0
+	}
+
+	// advance time
+	gen.t += Δt
+	gen.s += Δt
+}
+
+// Sine generates a sine wave
+func (gen *Generator) Sine(tʹ Timecode, f Frequency) Amplitude {
+	// advance the phasor
+	gen.advance(tʹ, f)
+	// return the 'sine' component of the phasor
+	return Amplitude(imag(gen.z))
+}
+
+// Cosine generates a sine wave
+func (gen *Generator) Cosine(tʹ Timecode, f Frequency) Amplitude {
+	// advance the phasor
+	gen.advance(tʹ, f)
+	// return the 'cosine' component of the phasor
+	return Amplitude(real(gen.z))
+}
+
+// Sawtooth generates a sawtooth wave
+func (gen *Generator) Sawtooth(tʹ Timecode, f Frequency) Amplitude {
+	// advance the phasor
+	gen.advance(tʹ, f)
+	// return the 'phase' of the phasor, normalized between [-1, 1]
+	return Amplitude(cmplx.Phase(gen.z) / math.Pi)
+}
+
+// Square generates a square wave
+func (gen *Generator) Square(tʹ Timecode, f Frequency) Amplitude {
+	// advance the phasor
+	gen.advance(tʹ, f)
+	// return the 'imaginary sign' component of the phasor
+	if math.Signbit(imag(gen.z)) {
+		return 1.0
+	}
+	return -1.0
+}
+
+// Triangle generates a sawtooth wave
+func (gen *Generator) Triangle(tʹ Timecode, f Frequency) Amplitude {
+	// advance the phasor
+	gen.advance(tʹ, f)
+	// return |Saw|, normalized between [-1, 1]
+	// 'rotate' by 90 degrees to align phase with Sine
+	saw := (cmplx.Phase(gen.z * complex(0, 1))) / math.Pi
+	return Amplitude(2*math.Abs(saw) - 1)
+}
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"math"
 	"time"
 
 	"github.com/gordonklaus/portaudio"
@@ -9,12 +8,13 @@ import (
 
 const sampleRate = Frequency(44100)
 
-var phasor *TrigGenerator
-var phasor2 *TrigGenerator
+var phasor SineGenerator
+var phasor2 TriangleGenerator
 
 func main() {
-	phasor = NewTrigGenerator(sampleRate)
-	phasor2 = NewTrigGenerator(sampleRate)
+
+	phasor = NewGenerator(sampleRate)
+	phasor2 = NewGenerator(sampleRate)
 
 	portaudio.Initialize()
 	defer portaudio.Terminate()
@@ -26,7 +26,7 @@ func main() {
 
 	defer stream.Close()
 	chk(stream.Start())
-	time.Sleep(10 * time.Second)
+	time.Sleep(4 * time.Second)
 	chk(stream.Stop())
 }
 
@@ -36,36 +36,12 @@ func processAudio(out [][]float32) {
 	for i := range out[0] {
 		var tc = Timecode(t + uint64(i))
 
-		df := phasor2.Sine(tc, Frequency(2)) * 100
+		df := phasor2.Triangle(tc, Frequency(0.5)) * 200
 
 		out[0][i] = float32(G2T(phasor.Sine, 350+Frequency(df))(tc))
 	}
 
 	t = t + uint64(len(out[0]))
-}
-
-var twoPi = 2 * math.Pi
-
-func sine(t Timecode, f Frequency) Amplitude {
-	var step = f / sampleRate
-	return Amplitude(math.Sin(twoPi * float64(t) * float64(step)))
-}
-
-func saw(t Timecode, f Frequency) Amplitude {
-	var step = f / sampleRate
-	var x = float64(t) * float64(step)
-	return Amplitude(2*(x-math.Floor(x)) - 1)
-}
-
-func square(t Timecode, f Frequency) Amplitude {
-	if math.Signbit(float64(sine(t, f))) {
-		return Amplitude(1)
-	}
-	return Amplitude(-1)
-}
-
-func triangle(t Timecode, f Frequency) Amplitude {
-	return Amplitude(2*math.Abs(float64(saw(t, f))) - 1)
 }
 
 func chk(err error) {
