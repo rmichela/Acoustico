c27a7e6cdaf7ba10d4365282dbc6a7d55ac93d0e
Ryan Michela <deltahat@gmail.com>
2017-03-21 22:20:39 -0700

    Resize PSD to width of terminal

--- raster.go
+++ raster.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/mjibson/go-dsp/spectral"
+	"github.com/wayneashleyberry/terminal-dimensions"
 )
 
 // Rasterizer fills one or more buffers with discreet audio samples
@@ -39,28 +40,30 @@ func RenderStereo(left TFunc, right TFunc) Rasterizer {
 
 // Visualize renders a Rasterizer's power spectral density to the screen
 func Visualize(sampleRate Frequency, colormap Colormap, inner Rasterizer) Rasterizer {
-	lowerAudible := Hz(20)
-	upperAudible := KHz(20)
+	termWidth := termWidth()
 
-	segmentLength := 256
+	segmentLength := termWidth*2 - 2 // FFT results in segmentLength/2+1 output samples
 
 	// stores the rendered output
 	lineQueueDepth := 8
 	lineQueue := make(chan []float64, lineQueueDepth)
 	go printer(lineQueue, segmentLength, colormap)
 
-	options := new(spectral.PwelchOptions)
-	options.Scale_off = true
-	options.Pad = segmentLength
+	options := newOptions(segmentLength)
 
 	return func(out [][]float64) {
+		// adjust PSD options if they are too agressive for the input data
+		if segmentLength > len(out[0]) {
+			options = newOptions(len(out[0]))
+		}
+
 		// compute raster values from the inner rasterizer
 		inner(out)
 
 		// divide the rasterized sound into [segmentLength] sample segments
 		var segments [][]float64
 		for _, chanBuf := range out {
-			chanSegs := spectral.Segment(chanBuf, segmentLength, 0)
+			chanSegs := spectral.Segment(chanBuf, options.NFFT, options.Noverlap)
 			segments = append(segments, chanSegs...)
 		}
 
@@ -79,13 +82,10 @@ func Visualize(sampleRate Frequency, colormap Colormap, inner Rasterizer) Raster
 
 		// normalize the PSD
 		for i := range psd {
-			// only render samples in the audible range
-			if float64(lowerAudible) < freqs[i] && freqs[i] < float64(upperAudible) {
-				// divide by number of segments to get average
-				psd[i] /= float64(len(segments))
-				// linearize the PSD
-				psd[i] *= math.Sqrt(freqs[i]) * 0.5
-			}
+			// divide by number of segments to get average
+			psd[i] /= float64(len(segments))
+			// linearize the PSD
+			psd[i] *= math.Sqrt(freqs[i])
 		}
 
 		// skip line rendering if there are too many lines waiting to be rendered
@@ -95,6 +95,16 @@ func Visualize(sampleRate Frequency, colormap Colormap, inner Rasterizer) Raster
 	}
 }
 
+func newOptions(segmentLength int) *spectral.PwelchOptions {
+	overlap := segmentLength / 2
+	options := new(spectral.PwelchOptions)
+	options.Scale_off = true
+	options.Noverlap = overlap
+	options.NFFT = segmentLength
+	options.Pad = segmentLength
+	return options
+}
+
 // Renders printed lines from a queue
 func printer(lineQueue chan []float64, segmentLength int, colormap Colormap) {
 	outLine := bytes.NewBuffer(make([]byte, 30*segmentLength, 30*segmentLength))
@@ -102,15 +112,33 @@ func printer(lineQueue chan []float64, segmentLength int, colormap Colormap) {
 		psd := <-lineQueue
 		outLine.Reset()
 		for i := range psd {
-			r, g, b := colormap(psd[i])
-			outLine.WriteString("\x1b[48;2;")
-			outLine.WriteString(strconv.Itoa(int(r)))
-			outLine.WriteString(";")
-			outLine.WriteString(strconv.Itoa(int(g)))
-			outLine.WriteString(";")
-			outLine.WriteString(strconv.Itoa(int(b)))
-			outLine.WriteString("m \x1b[0m")
+			// drop the lowest two frequency bins to to 1/f noise
+			// http://dsp.stackexchange.com/questions/540/lower-frequencies-stronger-in-fft
+			if i >= 2 {
+				r, g, b := colormap(psd[i])
+				outLine.WriteString("\x1b[48;2;")
+				outLine.WriteString(strconv.Itoa(int(r)))
+				outLine.WriteString(";")
+				outLine.WriteString(strconv.Itoa(int(g)))
+				outLine.WriteString(";")
+				outLine.WriteString(strconv.Itoa(int(b)))
+				outLine.WriteString("m \x1b[0m")
+			}
 		}
 		fmt.Println(outLine.String())
 	}
+}
+
+// get the width of the terminal - always even
+func termWidth() int {
+	w, e := terminaldimensions.Width()
+	if e != nil {
+		return 80
+	}
+
+	// make w even
+	if w%2 != 0 {
+		w--
+	}
+	return int(w)
 }
